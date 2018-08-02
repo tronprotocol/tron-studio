221289af5828c432c4236e7d29a5c0610e09fd80
huzhenyuan <huzhenyuan@github.com>
2018-08-02 20:15:51 +0800

    remove unused resource

--- src/main/java/org/tron/studio/MainApplication.java
+++ src/main/java/org/tron/studio/MainApplication.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
 public class MainApplication extends Application {
 
