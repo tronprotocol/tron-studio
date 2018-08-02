839986e650c070ff938125795420723fea337cb7
Guanghui Wang <wangguanghui0607@gmail.com>
2018-08-03 03:46:18 +0900

    Create new contract

--- src/main/java/org/tron/studio/ui/LeftCodeListController.java
+++ src/main/java/org/tron/studio/ui/LeftCodeListController.java
@@ -1,4 +1,40 @@
 package org.tron.studio.ui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LeftCodeListController {
+    static final Logger logger = LoggerFactory.getLogger(RightTabCompileController.class);
+    @FXML private TabPane codeAreaTabPane;
+    @FXML private Tab defualtTab;
+
+    @FXML
+    public void initialize() {
+        // Nothing
+    }
+
+    @FXML
+    private void addTab() {
+        logger.debug("addTab");
+        System.out.println(codeAreaTabPane);
+        int numTabs = codeAreaTabPane.getTabs().size();
+        logger.debug(Integer.toString(numTabs));
+        Tab tab = new Tab("Tab "+(numTabs+1));
+        codeAreaTabPane.getTabs().add(tab);
+    }
+
+    @FXML
+    private void listTabs() {
+        codeAreaTabPane.getTabs().forEach(tab -> System.out.println(tab.getText()));
+        System.out.println();
+    }
+
+    public void CreateContract(ActionEvent actionEvent) {
+        //tabPane.getTabs().add(new Tab("Tab 1"))
+        addTab();
+        listTabs();
+    }
 }
