301f950cc7b42af823262c039a1058813f6e0460
develop <develop@qq.com>
2018-08-24 01:13:28 +0800

    update log system

--- src/main/java/org/tron/studio/ui/CodeAreaController.java
+++ src/main/java/org/tron/studio/ui/CodeAreaController.java
@@ -1,11 +1,22 @@
 package org.tron.studio.ui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import lombok.extern.slf4j.Slf4j;
+import org.fxmisc.richtext.CodeArea;
+import org.tron.studio.ShareData;
+
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 
+@Slf4j(topic = "CodeAreaController")
 public class CodeAreaController {
+    public CodeArea codeArea;
+
     @PostConstruct
     public void initialize() throws IOException {
-
+        codeArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            ShareData.currentContractSourceCode.setValue(newValue);
+        });
     }
 }
