f278fab154d6cf7db91a9d13193ac57a1596deb7
hunter <hunter@qq.com>
2018-09-04 10:11:32 +0000

    add energy

--- src/main/java/org/tron/studio/ui/BottomController.java
+++ src/main/java/org/tron/studio/ui/BottomController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import lombok.extern.slf4j.Slf4j;
+import org.tron.api.GrpcAPI.AccountResourceMessage;
 import org.tron.protos.Protocol;
 import org.tron.studio.ShareData;
 
@@ -16,6 +17,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BottomController {
     public JFXButton nowBlockButton;
     public JFXButton balanceButton;
+    public JFXButton energyButton;
     public JFXButton rpcRestartButton;
 
     private ScheduledExecutorService syncExecutor = Executors.newSingleThreadScheduledExecutor();
@@ -26,22 +28,26 @@ public class BottomController {
 
         syncExecutor.scheduleWithFixedDelay(() -> {
             Protocol.Account account = null;
+            AccountResourceMessage accountResource = null;
             try {
                 if (ShareData.wallet == null || ShareData.wallet.getRpcCli() == null) {
                     return;
                 }
                 account = ShareData.wallet.queryAccount();
+                accountResource = ShareData.wallet
+                    .getAccountResource(account.getAddress().toByteArray());
             } catch (Exception e) {
                 logger.info("Connecting {}", counter.incrementAndGet());
                 return;
             }
-//            Protocol.Account.AccountResource resource = account.getAccountResource();
-//            Protocol.Account.Frozen frozenBalanceForEnergy = resource.getFrozenBalanceForEnergy();
+
             String balance = Long.toString(account.getBalance() / 1_000_000);
             String blockNumber = Long.toString(ShareData.wallet.getBlock(-1).getBlockHeader().getRawData().getNumber());
+            String energy =  Long.toString(accountResource.getEnergyLimit() -  accountResource.getEnergyUsed());
             Platform.runLater(() -> {
                 nowBlockButton.setText("Now Block:" + blockNumber);
                 balanceButton.setText("Balance:" + balance);
+                energyButton.setText("Energy Left:" + energy);
             });
         }, 2_000, 500, TimeUnit.MILLISECONDS);
     }
